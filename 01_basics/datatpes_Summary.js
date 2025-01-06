2e63e60bec9c414afcd6ed83b01c18041f1e59bb
Jaideep Singh <105032931+Jaideep302002@users.noreply.github.com>
2025-01-06 23:49:19 +0530

    Add files via upload

---
+++ 01_basics/datatpes_Summary.js
@@ -0,0 +1,37 @@
+//  Primitive
+
+//  7 types : String, Number, Boolea n, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+  name: "jaideep",
+  age: 22,
+};
+
+const myFunction = function () {
+  console.log("Hello world");
+};
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Stack (Primitive) -> In this we got a copy of a variable that we declared , Heap (Non-Primitive) -> In this we get the reference of the original value.
